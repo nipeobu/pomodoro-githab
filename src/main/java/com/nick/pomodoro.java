4d6c9615df6fb702498addadfd9bfde434383a61
nipeobu <107066719+nipeobu@users.noreply.github.com>
2022-06-12 19:35:11 +0300

    Fix problem (4)

--- src/main/java/com/nick/pomodoro.java
+++ src/main/java/com/nick/pomodoro.java
@@ -5,6 +5,6 @@ public class pomodoro {
     public static void main(String[] args) {
         System.out.println("Hello, pomodoro!");
         System.out.println("Hello, everyone!");
-        hfvjbjgvjhb;
+        System.out.println("Привее, Ура!");
     }
 }
