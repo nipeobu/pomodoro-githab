bda6b5e2a9b1ecef0054cedc228718fd307516a6
nipeobu <107066719+nipeobu@users.noreply.github.com>
2022-06-12 19:05:18 +0300

    3-th commit

--- src/main/java/com/nick/pomodoro.java
+++ src/main/java/com/nick/pomodoro.java
@@ -5,7 +5,5 @@ public class pomodoro {
     public static void main(String[] args) {
         System.out.println("Hello, pomodoro!");
         System.out.println("Hello, everyone!");
-        kjlklk
-
     }
 }
