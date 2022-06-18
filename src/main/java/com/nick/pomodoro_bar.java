970d13b8a491f1a1473bde7d97ff9631732a23d3
nipeobu <107066719+nipeobu@users.noreply.github.com>
2022-06-18 21:14:12 +0300

    Pomodoro fix input next 4

--- src/main/java/com/nick/pomodoro_bar.java
+++ src/main/java/com/nick/pomodoro_bar.java
@@ -115,9 +115,10 @@ public class pomodoro_bar {
             String x = String.format("%.1f", i * time * 1.0 / size);
             String percent = String.format("%.1f", i * 100.0 / size);
             System.out.print(process + percent + "% " + (" ").repeat(5 - percent.length()) + "[" + ("#").repeat(i) + ("-").repeat(size - i) + "]    ( " + x + "min / " + time + "min )" + "\r");
-            if (!isTest) {
-                TimeUnit.SECONDS.sleep(length);
-            }
+//            if (!isTest) {
+//                TimeUnit.SECONDS.sleep(length);
+//            }
+            TimeUnit.SECONDS.sleep(length);
         }
         System.out.println();
     }
